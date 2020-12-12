178813be3377045f4b7b1e658ed779d5509d8458
Bradley Carrion <bradley.carrion@gmail.com>
2020-12-11 23:02:19 -0800

    react and typescript

---
+++ src/components/Everything.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+
+const Everything = () => {
+    return <div className="introduction">Hello world</div>
+};
+
+export default Everything;
---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.tsx',
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Production',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
